3b50968d939af06fa085ad69a8ddd275fbb6895e
parkSeryu <pjh971212@gmail.com>
2024-04-16 01:38:45 +0900

    자바 기본 문법 1 (변수, 함수, if, for)

--- src/main/java/com/parkseryu/shop/ShopApplication.java
+++ src/main/java/com/parkseryu/shop/ShopApplication.java
@@ -8,6 +8,12 @@ public class ShopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
+
+        final String lover = "parkseryu";
+        System.out.println("Hello, " + lover + "!");
+        int age = 18;
+        System.out.println("I'm " + age + " years old.");
+
     }
 
 }
