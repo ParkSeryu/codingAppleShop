7a3aba6640bc517a399511c165f9f45f45436c05
parkSeryu <pjh971212@gmail.com>
2024-04-20 22:13:53 +0900

    DB 데이터 출력하려면

--- src/main/java/com/parkseryu/shop/ItemController.java
+++ src/main/java/com/parkseryu/shop/ItemController.java
@@ -1,14 +1,26 @@
 package com.parkseryu.shop;
 
+import java.util.ArrayList;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ItemController {
+
+    private final ItemRepository itemRepository;
+
+    public ItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
     @GetMapping("/list")
     String list(Model model) {
-        model.addAttribute("name", "홍길동");
+        var result = itemRepository.findAll();
+        System.out.println(result.get(0).price);
+
+        model.addAttribute("name", "비싼 바지");
         return "list.html";
     }
 }
