10dc488b4b770bcab931236ba971724b49563b86
parkSeryu <pjh971212@gmail.com>
2024-04-21 23:55:33 +0900

    상품 추가기능 1 (Map 자료형)

--- src/main/java/com/parkseryu/shop/Item.java
+++ src/main/java/com/parkseryu/shop/Item.java
@@ -6,10 +6,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/parkseryu/shop/ItemController.java
+++ src/main/java/com/parkseryu/shop/ItemController.java
@@ -1,9 +1,13 @@
 package com.parkseryu.shop;
 
 import com.parkseryu.homework.test;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ItemController {
@@ -29,7 +33,30 @@ public class ItemController {
         homework.addAge();
         homework.changeAge(12);
         homework.changeAge(-10);
-
         return "list";
     }
+
+    @GetMapping("/write")
+    String write() {
+        return "write";
+
+    }
+
+    @PostMapping("/add")
+    String addPost(@RequestParam Map formData) {
+        Map<String, String> test = new HashMap<>();
+        test.put("title", (String) formData.get("title"));
+        test.put("price", (String) formData.get("price"));
+        System.out.println(test);
+
+        Item item = new Item();
+        item.setTitle(test.get("title"));
+        item.setPrice(Integer.valueOf(test.get("price")));
+
+        itemRepository.save(item);
+
+        return "redirect:/list";
+    }
+
+
 }
