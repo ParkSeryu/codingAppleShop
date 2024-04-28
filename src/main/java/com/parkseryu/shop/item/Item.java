7ee3e6416fd2ec4b307e749e1fd4c7979d50ea04
parkSeryu <pjh971212@gmail.com>
2024-04-28 19:14:54 +0900

    댓글기능 1 ~ 2 (Normalization)

--- src/main/java/com/parkseryu/shop/item/Item.java
+++ src/main/java/com/parkseryu/shop/item/Item.java
@@ -6,12 +6,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
+@ToString
+@Table(indexes = @Index(columnList = "title", name = "작명"))
+
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
